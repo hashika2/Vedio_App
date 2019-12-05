e58c637d642c526a015b1d058089b21ce319f348
hashika <m.g.hashikamaduranga@gmail.com>
2019-12-05 22:09:59 +0530

    fix the error adding exception handling

--- vedio/src/component/App.js
+++ vedio/src/component/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import axios from 'axios';
 
 class App extends React.Component{
-    onTermSubmit=async (term)=>{
-       const response =await youtube.get('/search',{
-            params:{
-                q:term
-            }
-        });
-        console.log(response)
+        onTermSubmit=async (term)=>{
+            const APIKEY = 'AIzaSyAQpXGoCMfxA2RPAQoz5Qqa5qYhpxBYsI0';
+
+        try{
+            const response=await axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params:{
+                    q:term,
+                    part:'snippet',
+                    maxResults:5,
+                    key:APIKEY
+                }
+            });
+            console.log(response)
+        }catch(err){
+            console.error(err.message);
+        }
+        
      }
      
     render(){
